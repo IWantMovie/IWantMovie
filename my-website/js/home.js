e58d35bb5bd5bd8e76ed18a04e6c1f5ed0486dc5
IWantMovie <mercadojerome1205@gmail.com>
2025-04-30 17:35:03 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '69b16d393dc8be6c3f6f4ea591d1d7e8';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
